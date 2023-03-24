7fab2cbcd52d29266bdce4d14493f4f3bee9581a
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2023-03-24 11:05:11 -0300

    Create dark-mode in main-menu css file

--- js/script.js
+++ js/script.js
@@ -20,7 +20,10 @@ for (var i = 0; i < iconBx.length; i++) {
 
 const html = document.querySelector("html");
 const togglee = document.getElementById("toggle");
+const menu = document.getElementById("menu");
+
 togglee.onclick = function () {
   togglee.classList.toggle("active");
+  menu.classList.toggle("active");
   html.classList.toggle("dark-mode");
 };
